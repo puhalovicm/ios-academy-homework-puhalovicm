3aeeee8b3cfb74c2ece37a09be9903bbcbc9f7d5
Mateo Puhalovic <mateo.puhalovic@gmail.com>
2021-07-23 19:28:54 +0200

    Fix styling issues

--- TV Shows/TV Shows/Login/LoginViewController.swift
+++ TV Shows/TV Shows/Login/LoginViewController.swift
@@ -16,8 +16,7 @@ final class LoginViewController: UIViewController {
     @IBOutlet private weak var registerButton: UIButton!
     @IBOutlet private weak var usernameTextField: UITextField!
     @IBOutlet private weak var passwordTextField: UITextField!
-    @IBOutlet weak var rememberMeButton: UIButton!
-    @IBOutlet weak var scrollView: UIScrollView!
+    @IBOutlet private weak var rememberMeButton: UIButton!
     
     private var username: String = ""
     private var password: String = ""
@@ -26,84 +25,72 @@ final class LoginViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        
-        updateButtons()
+
+        setButtonsEnabled(isEnabled: isInputValid)
         setupProgressHUD()
         setupButtons()
         setupUsernameTextField()
         setupPasswordTextField()
-        NotificationCenter.default.addObserver(self, selector: #selector(self.onOrientationChange), name: UIDevice.orientationDidChangeNotification, object: nil)
-        scrollView.bounces = false
+        NotificationCenter.default.addObserver(self, selector: #selector(onOrientationChange), name: UIDevice.orientationDidChangeNotification, object: nil)
     }
 
     deinit {
        NotificationCenter.default.removeObserver(self, name: UIDevice.orientationDidChangeNotification, object: nil)
     }
+}
+
+private extension LoginViewController {
 
     @objc func onOrientationChange() {
         setupUsernameTextField()
         setupPasswordTextField()
     }
-    
+
     @IBAction func onShowPasswordClicked(_ showPasswordButton: UIButton!) {
         showPassword = !showPassword
-        if showPassword {
-            self.passwordTextField.isSecureTextEntry = false
-            setShowPasswordButtonImage(showPasswordButton: showPasswordButton)
-        } else {
-            self.passwordTextField.isSecureTextEntry = true
-            setDoNotShowPasswordButtonImage(showPasswordButton: showPasswordButton)
-        }
+        passwordTextField.isSecureTextEntry = !showPassword
+        setShowPassword(showPasswordButton: showPasswordButton, shouldShow: showPassword)
     }
-    
+
     @IBAction func onButtonClicked(_ sender: Any) {
         printMessage()
         print(String(format: "Username: %1$@\nPassword: %2$@\nRememberMe: %3$@", username, password, rememberMeSelected ? "YES" : "NO"))
     }
-    
-    
+
     @IBAction func onRememberMeSelected(_ sender: Any) {
         rememberMeSelected = !rememberMeSelected
-        if rememberMeSelected {
-            setRememberMeSelectedImage()
-        } else {
-            setRememberMeUnselectedImage()
-        }
+        setRememberMe(isSelected: rememberMeSelected)
     }
-    
-    @IBAction func onUsernameChanged(_ sender: Any) {
-        username = self.usernameTextField.text ?? ""
-        updateButtons()
-    }
-    
-    @IBAction func onPasswordChanged(_ sender: Any) {
-        password = self.passwordTextField.text ?? ""
-        updateButtons()
-    }
-}
 
-private extension LoginViewController {
+    @IBAction func onUsernameChanged(_ sender: Any) {
+        username = usernameTextField.text ?? ""
+        setButtonsEnabled(isEnabled: isInputValid)
+    }
+
+    @IBAction func onPasswordChanged(_ sender: Any) {
+        password = passwordTextField.text ?? ""
+        setButtonsEnabled(isEnabled: isInputValid)
+    }
     
     func setupPasswordTextField() {
-        setBottomLine(textField: self.passwordTextField)
-        setLeftPaddingView(textField: self.passwordTextField)
-        self.passwordTextField.rightView = createShowPasswordButton()
-        self.passwordTextField.rightViewMode = .always
-        
+        setBottomLine(textField: passwordTextField)
+        setLeftPaddingView(textField: passwordTextField)
+        passwordTextField.rightView = createShowPasswordButton()
+        passwordTextField.rightViewMode = .always
     }
     
     func setupUsernameTextField() {
-        setBottomLine(textField: self.usernameTextField)
-        setLeftPaddingView(textField: self.usernameTextField)
-        setRightPaddingView(textField: self.usernameTextField)
+        setBottomLine(textField: usernameTextField)
+        setLeftPaddingView(textField: usernameTextField)
+        setRightPaddingView(textField: usernameTextField)
     }
     
     func setBottomLine(textField: UITextField, height: CGFloat = 1.0) {
         textField.layer.masksToBounds = true
         let usernameBottomLine = CALayer()
-        usernameBottomLine.frame = CGRect(x: 0.0, y: textField.frame.height - height, width: textField.frame.width, height: height)
+        usernameBottomLine.frame = CGRect(x: 0.0, y: textField.frame.height - height, width: view.frame.width - 40, height: height)
         usernameBottomLine.backgroundColor = UIColor.white.cgColor
-        textField.borderStyle = UITextField.BorderStyle.none
+        textField.borderStyle = .none
         textField.layer.addSublayer(usernameBottomLine)
     }
     
@@ -123,27 +110,23 @@ private extension LoginViewController {
         let showPasswordButton = UIButton(type: .custom)
         showPasswordButton.setImage(UIImage(named: "ic-invisible"), for: .normal)
         showPasswordButton.contentEdgeInsets = UIEdgeInsets(top: 0, left: 10, bottom: 0, right: 10)
-        showPasswordButton.addTarget(self, action: #selector(self.onShowPasswordClicked), for: .touchUpInside)
+        showPasswordButton.addTarget(self, action: #selector(onShowPasswordClicked), for: .touchUpInside)
         return showPasswordButton
-    }
-    
-    func updateButtons() {
-        setButtonsEnabled(isEnabled: checkLoginParams())
     }
     
     func setButtonsEnabled(isEnabled: Bool) {
         loginButton.isEnabled = isEnabled
         registerButton.isEnabled = isEnabled
         
-        if (isEnabled) {
-            loginButton.backgroundColor = UIColor.white
+        if isEnabled {
+            loginButton.backgroundColor = .white
         } else {
-            loginButton.backgroundColor = UIColor.white.withAlphaComponent(0.3)
+            loginButton.backgroundColor = .white.withAlphaComponent(0.3)
         }
     }
     
-    func checkLoginParams() -> Bool {
-        return !username.isEmpty && !password.isEmpty
+    var isInputValid: Bool {
+        !username.isEmpty && !password.isEmpty
     }
     
     func printMessage() {
@@ -166,28 +149,16 @@ private extension LoginViewController {
             SVProgressHUD.dismiss()
         }
     }
-    
-    func setShowPasswordButtonImage(showPasswordButton: UIButton) {
-        if let image = UIImage(named: "ic-visible") {
-            showPasswordButton.setImage(image, for: .normal)
-        }
-    }
-    
-    func setDoNotShowPasswordButtonImage(showPasswordButton: UIButton) {
-        if let image = UIImage(named: "ic-invisible") {
-            showPasswordButton.setImage(image, for: .normal)
-        }
-    }
-    
-    func setRememberMeSelectedImage() {
-        if let image = UIImage(named: "ic-checkbox-selected") {
-            self.rememberMeButton.setImage(image, for: .normal)
-        }
+
+    func setShowPassword(showPasswordButton: UIButton, shouldShow: Bool) {
+        let imageName = shouldShow ? "ic-visible" : "ic-invisible"
+        guard let image = UIImage(named: imageName) else { return }
+        showPasswordButton.setImage(image, for: .normal)
     }
 
-    func setRememberMeUnselectedImage() {
-        if let image = UIImage(named: "ic-checkbox-unselected") {
-            self.rememberMeButton.setImage(image, for: .normal)
-        }
+    func setRememberMe(isSelected: Bool) {
+        let imageName = isSelected ? "ic-checkbox-selected" : "ic-checkbox-unselected"
+        guard let image = UIImage(named: imageName) else { return }
+        rememberMeButton.setImage(image, for: .normal)
     }
 }
